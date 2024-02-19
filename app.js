98ac8e0a2d5d10c3e126529d45c46296c1786106
Patrick Lemes <patrickflemes@gmail.com>
2024-02-18 22:31:02 -0300

    Alteração mensagem h1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto)
 
 function exibirMensagemInicial()
 {
-    exibirTextoNaTela("h1", "Jogo do número secreto");
+    exibirTextoNaTela("h1", "Número Secreto");
     exibirTextoNaTela("p", "Escolha um número entre 1 e 10");
 }
 
